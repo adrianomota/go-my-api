cffd3e481ebdef45d9b13eed8771d85151e56b5d
Adriano Mota <adrianowsh@hotmail.com>
2024-03-10 12:12:00 -0300

    add swagger support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/adrianomota/fullcycle/my-api/configs"
+	_ "github.com/adrianomota/fullcycle/my-api/docs"
 	"github.com/adrianomota/fullcycle/my-api/internal/entity"
 	"github.com/adrianomota/fullcycle/my-api/internal/infra/database"
 	"github.com/adrianomota/fullcycle/my-api/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+// @title My Golang API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 
 func main() {
 	config, err := configs.LoadConfig(".")
@@ -35,6 +53,7 @@ func main() {
 
 	//middlewares
 	r.Use(middleware.Logger)
+	r.Use(MyLogRequest)
 
 	//products
 	r.Route("/products", func(r chi.Router) {
@@ -54,6 +73,16 @@ func main() {
 		r.Post("/token", userHandler.GetJWT)
 	})
 
+	//swagger
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
+
 	//server
 	http.ListenAndServe(":8000", r)
+}
+
+func MyLogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("My Request: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/infra/webserver/handlers/user_handler.go
+++ internal/infra/webserver/handlers/user_handler.go
@@ -12,6 +12,10 @@ import (
 	"github.com/go-chi/jwtauth"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB       database.UserInterface
 	Jwt          *jwtauth.JWTAuth
@@ -25,6 +29,17 @@ func NewUserHandler(db database.UserInterface, jwt *jwtauth.JWTAuth, jwtExpiresI
 	}
 }
 
+// GetJWT godoc
+// @Summary 			Get a user JWT
+// @Description  	Get a user JWT
+// @Tags					users
+// @Accept				json
+// @Produce				json
+// @Param					request		body				dto.GetJwtInput 	true		"user credentials"
+// @Success				200				{object} 		dto.GetJwtOutput
+// @Failure				404
+// @Failure				500				{object}		Error
+// @Router				/users/token	[post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	var user dto.GetJwtInput
 	err := json.NewDecoder(r.Body).Decode(&user)
@@ -47,17 +62,23 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
 	})
 
-	accessToken := struct {
-		AccessToken string `json:"access_token"`
-	}{
-		AccessToken: tokenString,
-	}
+	accessToken := dto.GetJwtOutput{AccessToken: tokenString}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(accessToken)
 }
 
+// Create user godoc
+// @Summary 			Create User
+// @Description  	Create User
+// @Tags					users
+// @Accept				json
+// @Produce				json
+// @Param					request		body			dto.CreateUserInput 	true		"user request"
+// @Success				201
+// @Failure				500				{object}	Error
+// @Router				/users	[post]
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
@@ -75,6 +96,8 @@ func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	err = h.UserDB.Create(u)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		errorMessage := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(errorMessage)
 		return
 	}
 
@@ -85,11 +108,14 @@ func (h *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	if id == "" {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Invalid param")
 		return
 	}
 	user, err := h.UserDB.FindById(id)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
+		errorMessage := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(errorMessage)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
